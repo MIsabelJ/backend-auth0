cf99e2d246d3bcae467166f6b810d2644f071a89
MIsabelJ <129914062+MIsabelJ@users.noreply.github.com>
2024-06-13 09:07:39 -0300

    Add backend files

---
+++ src/main/java/com/utn/backendAuth0/BackendAuth0Application.java
@@ -0,0 +1,13 @@
+package com.utn.backendAuth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAuth0Application.class, args);
+	}
+
+}
